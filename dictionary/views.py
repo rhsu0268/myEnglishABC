889b6785f72f8cd68428922290f944aff1058d14
Richard Hsu <rhsu@gwu.edu>
2016-03-03 02:42:12 -0500

    Added template to dictionary

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.shortcuts import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You are at the dictionary index.")
+    #return HttpResponse("Hello, world. You are at the dictionary index.")
+    template = loader.get_template('dictionary/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
 
 def detail(request, sentence_id):
 	return HttpResponse("You are loking at sentence %s." % sentence_id)
