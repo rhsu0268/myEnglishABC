03ffa427d013dcd8df5ace348fb9035a3080e3c2
Richard Hsu <rhsu@gwu.edu>
2016-03-06 11:08:58 -0500

    Pulled in js files

--- myEnglishABC/settings.py
+++ myEnglishABC/settings.py
@@ -139,4 +139,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), os.path.join(BASE_DIR, "images"),)
+
+
