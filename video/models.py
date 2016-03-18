224aa5a199bd102b6d713c27f291f251c8f8bf9c
Richard Hsu <rhsu@gwu.edu>
2016-03-18 16:27:19 -0400

    Successfully dispayling list of videos

--- video/models.py
+++ video/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class Video(models.Model):
     video_link = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.video_link
--- video/views.py
+++ video/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.shortcuts import loader
 
+from .models import Video
 # Create your views here.
 
 def index(request):
     #return HttpResponse("Hello, world. You are at the dictionary index.")
-    template = loader.get_template('video/index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+
+    videos = Video.objects.all()
+    output = ', '.join([str(video) for video in videos])
+    return HttpResponse(output)
+    #template = loader.get_template('video/index.html')
+    #context = {}
+    #return HttpResponse(template.render(context, request))
