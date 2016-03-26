6937ba640bcc2f41eda12b1804fe8096095d02b8
Richard Hsu <rhsu@gwu.edu>
2016-03-26 19:30:34 -0400

    Added date filter to the footer

--- myEnglishABC/views.py
+++ myEnglishABC/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.shortcuts import loader
 
+from datetime import datetime
+
 def index(request):
     #return HttpResponse("Hello, world. You are at the homepage of the application")
-   return render(request, 'index.html')
+   return render(request, 'index.html', {'time': datetime.now() })
