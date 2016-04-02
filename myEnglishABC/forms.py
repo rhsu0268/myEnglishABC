dc230acef0739638f34d6e62a8b90fc01a08a8ca
Richard Hsu <rhsu@gwu.edu>
2016-04-02 17:46:34 -0400

    Added a clean form function

--- myEnglishABC/forms.py
+++ myEnglishABC/forms.py
@@ -8,7 +8,8 @@ def must_be_empty(value):
 
 class SuggestionForm(forms.Form):
 	name = forms.CharField()
-	email = forms.EmailField(validators=[must_be_empty])
+	email = forms.EmailField()
+	verify_email = forms.EmailField(label='Please verify your email address:')
 	suggestion = forms.CharField(widget=forms.Textarea, validators=[validators.MaxLengthValidator(10)])
 	honeypot = forms.CharField(required=False, 
 								widget=forms.HiddenInput, 
@@ -21,3 +22,10 @@ class SuggestionForm(forms.Form):
 	# 			"honeypot should be left empty. Bad bot!")
 	# 	return honeypot
 
+	def clean(self):
+		cleaned_data = super().clean()
+		email = cleaned_data['email']
+		verify = cleaned_data['verify_email']
+
+		if email != verify:
+			raise forms.ValidationError('You need to enter the same email in both fields!')
