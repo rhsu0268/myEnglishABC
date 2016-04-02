07ee952b9ae6b873de8073037b3ad86fda7160d3
Richard Hsu <rhsu@gwu.edu>
2016-04-01 22:49:27 -0400

    Added honeypot

--- myEnglishABC/forms.py
+++ myEnglishABC/forms.py
@@ -5,3 +5,12 @@ class SuggestionForm(forms.Form):
 	name = forms.CharField()
 	email = forms.EmailField()
 	suggestion = forms.CharField(widget=forms.Textarea)
+	honeypot = forms.CharField(required=False, 
+								widget=forms.HiddenInput, 
+								label="Leave empty")
+	def clean_honeyput(self):
+		honeyput = self.cleaned_data['honeypot']
+		if len(honeypot):
+			raise forms.ValidationError(
+				"honeypot should be left empty. Bad bot!")
+		return honeypot
