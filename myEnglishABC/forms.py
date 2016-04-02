869f64ca1a4189aa53327a0e86dcc4f844a822a2
Richard Hsu <rhsu@gwu.edu>
2016-04-02 17:30:05 -0400

    Added some form validators

--- myEnglishABC/forms.py
+++ myEnglishABC/forms.py
@@ -1,16 +1,23 @@
 from django import forms
+from django.core import validators
 
+
+def must_be_empty(value):
+	if value:
+		raise forms.ValidationError('is not empty')
 
 class SuggestionForm(forms.Form):
 	name = forms.CharField()
-	email = forms.EmailField()
-	suggestion = forms.CharField(widget=forms.Textarea)
+	email = forms.EmailField(validators=[must_be_empty])
+	suggestion = forms.CharField(widget=forms.Textarea, validators=[validators.MaxLengthValidator(10)])
 	honeypot = forms.CharField(required=False, 
 								widget=forms.HiddenInput, 
-								label="Leave empty")
-	def clean_honeyput(self):
-		honeyput = self.cleaned_data['honeypot']
-		if len(honeypot):
-			raise forms.ValidationError(
-				"honeypot should be left empty. Bad bot!")
-		return honeypot
+								label="Leave empty",
+								validators=[validators.MaxLengthValidator(0)])
+	# def clean_honeyput(self):
+	# 	honeyput = self.cleaned_data['honeypot']
+	# 	if len(honeypot):
+	# 		raise forms.ValidationError(
+	# 			"honeypot should be left empty. Bad bot!")
+	# 	return honeypot
+
