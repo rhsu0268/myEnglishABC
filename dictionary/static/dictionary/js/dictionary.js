7fe38ee6f24bf2aa3089b1859a0888a9be1d6e44
Richard Hsu <rhsu@gwu.edu>
2016-04-10 11:43:56 -0400

    Added code to make ajax call to googletransalate

--- dictionary/static/dictionary/js/dictionary.js
+++ dictionary/static/dictionary/js/dictionary.js
@@ -1,5 +1,30 @@
 console.log("On the dictionary page!");
 
 $( "#translate_text" ).click(function() {
-  console.log("Translate button is clicked!");
+	console.log("Translate button is clicked!");
+
+	var sentence = $('#sentence').val();
+	console.log(sentence);
+	if (!sentence || sentence == "" || sentence == "Enter your sentence here.")
+	{
+		console.log("Please enter some text!");
+		return;
+	}
+	else 
+	{
+  		var translateAPI = "https://www.googleapis.com/language/translate/v2?key=AIzaSyDyXIh9jVvbng3U7mmnte1yGoEsLYB4RmA&source=en&target=zh-TW&q=" + sentence;
+  		$.getJSON( translateAPI, {
+    	
+    		//format: "json"
+  		})
+    	.done(function( data ) {
+   			console.log(data);
+
+   			$.each( data.data.translations, function( i, item ) {
+        		//$( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+        		console.log(item);
+      		});
+    	});
+    }
+	
 });
