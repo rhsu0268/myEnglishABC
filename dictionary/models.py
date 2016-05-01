ea1d3e39266761f0d5552bba9a8cc0173b165eda
Richard Hsu <rhsu@gwu.edu>
2016-05-01 19:04:39 -0400

    Made markup for homepage

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,5 +1,6 @@
 import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
 from django.utils import timezone
 # Create your models here.
@@ -7,6 +8,7 @@ from django.utils import timezone
 class Sentence(models.Model):
     sentence_text = models.CharField(max_length=2000)
     pub_date = models.DateTimeField('date published')
+    user = models.ForeignKey(User, unique=True)
 
     def __str__(self):
     	return self.sentence_text
