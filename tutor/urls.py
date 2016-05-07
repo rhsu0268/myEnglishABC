5c82873da3c3bf22a16380f2ad04027115c85d71
Richard Hsu <rhsu@gwu.edu>
2016-05-06 23:25:35 -0400

    Fixing problems with url

--- tutor/urls.py
+++ tutor/urls.py
@@ -3,9 +3,8 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'tutor'
-
 urlpatterns = [
 
-    url(r'^', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
   
 ]
