bd63c452de431e33182f293955490ed7384cf66b
Richard Hsu <rhsu@gwu.edu>
2016-05-19 22:51:11 -0400

    Fixing ajax request

--- dictionary/views.py
+++ dictionary/views.py
@@ -72,8 +72,8 @@ def saveWord(request):
     now = datetime.datetime.now()
     current_user = request.user
     resp = "The word has successfully been saved!"
-    #if request.is_ajax():
-    if request.method == 'POST':
+    if request.is_ajax():
+    #if request.method == 'POST':
         #chinese_text = request.POST.get('chinese_text')
         #unicode_text = chinese_text.encode('unicode-escape')
         #print(unicode_text)
