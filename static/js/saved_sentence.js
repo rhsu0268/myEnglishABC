b8e1c0933077bd39f2bd596415623b1876642e30
Richard Hsu <rhsu@gwu.edu>
2016-05-22 18:43:18 -0400

    Added some js

--- static/js/saved_sentence.js
+++ static/js/saved_sentence.js
@@ -5,4 +5,21 @@ $( '.success' ).click(function() {
 	console.log("say was clicked!");
 	console.log(this.id);
 
+	var button_id = this.id;
+	console.log(button_id);
+
+	$.ajax({
+
+    	url: "/dictionary/sayWord/",
+    	type: "POST",
+    	data: { id: button_id, 
+            csrfmiddlewaretoken: csrftoken
+    }
+
+  	}).done(function(data) {
+    	console.log("success");
+    	alert("success");
+
+  	}); 
+
 });
