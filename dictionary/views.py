7d3750865f4fa025c6805d998ca3e395791c774f
Richard Hsu <rhsu@gwu.edu>
2016-05-22 17:23:49 -0400

    Fixed saveSentence

--- dictionary/views.py
+++ dictionary/views.py
@@ -88,15 +88,15 @@ def saveWord(request):
         #audio_file = File(f)
 
         # make a string for the filename
-        id_string = makeId()
+        #id_string = makeId()
         #print(id_string)
-        file_string = 'audio-' + id_string + '.mp3'
+        #file_string = 'audio-' + id_string + '.mp3'
         #print(file_string)
         #tts = gTTS(text=text, lang='en')
         #tts.save(file_string)
 
 
-        sentence = Sentence(sentence_text=request.POST.get('text'), chinese_text=unicode_text, audio_filename=file_string, pub_date=now, user=current_user)
+        sentence = Sentence(sentence_text=request.POST.get('text'), chinese_text=unicode_text, audio_filename="test", pub_date=now, user=current_user)
         sentence.save()
         #sentence.text_recording.save('new', audio_file)
         #audio_fiel.close()
