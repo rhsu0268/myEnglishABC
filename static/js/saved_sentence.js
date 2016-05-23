32d46238a5782ffebd9630e4afb084e08a14858a
Richard Hsu <rhsu@gwu.edu>
2016-05-22 21:53:00 -0400

    Fixed url 3

--- static/js/saved_sentence.js
+++ static/js/saved_sentence.js
@@ -12,7 +12,7 @@ $( '.success' ).click(function() {
 
 	$.ajax({
 
-    	url: "/dictionary/sayWord/" + button_id,
+    	url: "/dictionary/sayWord/",
     	type: "POST",
     	data: { id: button_id, 
             csrfmiddlewaretoken: csrftoken
