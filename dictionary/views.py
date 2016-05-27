35bdfb527d0204fcb4f1f11a6014753318adc11c
Richard Hsu <rhsu@gwu.edu>
2016-05-26 22:19:47 -0400

    Trying to get objects of user

--- dictionary/views.py
+++ dictionary/views.py
@@ -55,7 +55,7 @@ def saveSentence(request):
 def showWords(request):
     if request.user.is_authenticated():
     	resp = "These are your saved words!"
-    	sentences = Sentence.objects.filter(user=request.user.get_username())
+    	sentences = Sentence.objects.filter(user_id=request.user)
     	for sentence in sentences:
     		# unicode_text = sentence.chinese_text.encode('unicode-escape')
     		# print(unicode_text)
